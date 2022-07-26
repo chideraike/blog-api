bbeb00341b0b4220c9fab379b79901d29ed53b5e
Chidera Ike <chideraike.s@gmail.com>
2022-07-26 16:41:08 +0100

    :card_file_box: store blog to db

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
 
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post("/blog")
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
