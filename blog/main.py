1804fc64b4ac16641b85a664f482bb7aef846f1e
Chidera Ike <chideraike.s@gmail.com>
2022-07-26 17:45:17 +0100

    :sparkles: added exception with status code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 
 from . import schemas, models
@@ -17,8 +17,8 @@ def get_db():
         db.close()
 
 
-@app.post("/blog")
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def create_a_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
@@ -32,7 +32,14 @@ def get_all_blogs(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get("/blog/{id}")
-def get_blog_by_id(id, db: Session = Depends(get_db)):
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+def get_blog_by_id(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+        )
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail": f"Blog with id {id} not found"}
+
     return blog
